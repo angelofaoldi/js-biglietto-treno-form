2b11584d108576772ba1972760af4a9ffe29bb6d
angelofaoldi <faoldiangelo@gmail.com>
2024-05-02 07:03:50 +0200

    JS: scontistica

--- js/main.js
+++ js/main.js
@@ -7,6 +7,24 @@ const eta = document.getElementById('eta');
 // BOTTONE che scatenerà l'azione
 const invia = document.getElementById('Invia')
 
+// bisogna rendere il bottone in ascolto, sensibile al click
+
+invia.addEventListener('click', function () {
+
+    const distanzaKm = Number(distanza.value);
+    const etaUtente = Number(eta.value);
+
+    // VARIABILE PREZZO 0.21
+    const prezzoKm = 0.21;
+
+    //SCONTISTICA
+    const scontoGiovani = 20;
+    const scontoAnziani = 40;
+
+    
+
+})
+
 
 
 
